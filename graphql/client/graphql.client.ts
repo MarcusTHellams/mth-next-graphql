8ad8a89e380145d18410abcb94148a8b12414c99
Marcus Hellams <mhellams@hotmail.com>
2023-02-08 16:12:12 -0500

    doing a log

--- graphql/client/graphql.client.ts
+++ graphql/client/graphql.client.ts
@@ -1,4 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
+console.log(process.env.API_HOST);
 
 export const graphqlClient = new GraphQLClient(`${process.env.API_HOST || ''}/api/graphql`);
